c9a542c08f95aebb9ffbf01959746520ac87fa1b
ReinUsesLisp <reinuseslisp@airmail.cc>
2019-12-09 03:21:57 -0300

    Add IADD CC tests

--- source/compute_tests.c
+++ source/compute_tests.c
@@ -205,6 +205,9 @@ static struct compute_test_descriptor const test_descriptors[] =
 	TEST("IADD_R.SAT HIGH",             0x7fffffff, iadd_r_high_sat,           8),
 	TEST("IADD_R.SAT LOW",              0x80000000, iadd_r_low_sat,            8),
 	TEST("IADD_R.SAT FAKE",             0xd0000000, iadd_r_fake_sat,           8),
+	TEST("IADD_R.CC ZERO",              0x00000001, iadd_r_cc_zero,            8),
+	TEST("IADD_R.CC SIGN",              0x9161ff02, iadd_r_cc_sign,            8),
+	TEST("IADD_R.CC CARRY",             0x00000604, iadd_r_cc_carry,           8),
 
 	ETEST("SUST.P.RGBA", 0x40f00000, sust_p_rgba, 8),
 	ETEST("SULD.P.RGBA", 0x42140000, suld_p_rgba, 8),
