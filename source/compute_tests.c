4c066763054afedfa5c0d70bfc3710325e927bef
ReinUsesLisp <reinuseslisp@airmail.cc>
2019-12-18 02:29:31 -0300

    Add LDG.U8 tests

--- source/compute_tests.c
+++ source/compute_tests.c
@@ -246,6 +246,8 @@ static struct compute_test_descriptor const test_descriptors[] =
 	TEST("SSY",                         0xa0f943de, ssy,                       8),
 	TEST("BRK",                         0xbabadead, brk,                       8),
 	TEST("SSY & BRK",                   0x0000dddd, ssy_brk,                   8),
+	TEST("LDG.E.CI.U8",                 0x000000f2, ldg_e_ci_u8,               8),
+	TEST("LDG.E.CI.U8 Unaligned",       0x000000f0, ldg_e_ci_u8_unaligned,     8),
 
 	ETEST("SUST.P.RGBA", 0x40f00000, sust_p_rgba, 8),
 	ETEST("SULD.P.RGBA", 0x42140000, suld_p_rgba, 8),
